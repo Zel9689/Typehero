48cc5ac467830a10322e0b46d14894a81254e4e9
zel9689 <fishxd1096@gmail.com>
2019-12-24 16:25:20 +0800

    update pictures, monster can move up and down now

--- TypeHero.py
+++ TypeHero.py
@@ -14,18 +14,42 @@ path = os.path.split(os.path.abspath(__file__))[0]
 background.fill((125,125,125))
 background = background.convert()
 
-#monsters
+#words
+
+#player picture
+
+#player's bullet
+
+#player health
+
+#monsters picture
 monster = pygame.image.load(os.path.join(path, 'monster01.png'))
 monster = monster.convert()
+monsterRect = monster.get_rect()
+monsterHeight = monsterRect.height
+monsterWidth = monsterRect.width
+print('height= ', monsterHeight)
+print('width= ', monsterWidth)
+monsterRect.center = (800,500) #微調怪物位置
+x, y = monsterRect.topleft
+print(x,y)
+dy = 3 #移動速度
+print(monsterRect.top)
+print(monsterRect.bottom)
+
+#monsters health
+
 
 #ball(example code)
+'''
 ball = pygame.Surface((30,30))
 ball.fill((255,255,255))
 pygame.draw.circle(ball, (0,0,255), (15,15), 15, 0)
 rect1 = ball.get_rect()
-rect1.center = (320,45)
+rect1.center = (26,300)
 x, y = rect1.topleft
 dx = 3
+'''
 
 #game clock
 clock = pygame.time.Clock()
@@ -38,11 +62,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.blit(background, (0,0))
-    screen.blit(monster, (0,0))
-    x += dx
-    rect1.center = (x, y)
-    if (rect1.left<=0 or rect1.right>=screen.get_width()):
-        dx *= -1
-    screen.blit(ball,rect1.topleft)
+
+#moving monster
+    y += dy
+    monsterRect.center = (x, y)
+    if (monsterRect.top<=0 or monsterRect.bottom>=screen.get_height()):
+        dy *= -1
+    screen.blit(monster,monsterRect.topleft)
+
     pygame.display.update()
 pygame.quit()
