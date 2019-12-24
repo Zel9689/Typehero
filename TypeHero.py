2a80e4411f37d5a6ffb3474d3a141c0b395c7c9a
zel9689 <fishxd1096@gmail.com>
2019-12-25 01:53:43 +0800

    find a bug: removeText fuction delete wrong the index of data, should just send index into fuction

--- TypeHero.py
+++ TypeHero.py
@@ -4,7 +4,11 @@ import os
 import random
 
 #Todo 字跑出的時機
-#Todo 字超過左邊就沒機會了
+#控制角色移動(上下)
+#角色打對字射出子彈(自動瞄準)
+#角色被字碰到會扣血
+#魔王的血量
+#同時跑出畫面外的字會有問題 吧?
 
 #文字隨機出和隨機角度
 def randomWordsAngle():
@@ -13,22 +17,40 @@ def randomWordsAngle():
     Text_dy.insert(0, random.randint(-2,2)) #Text_dy -2~2 隨機 插入index 0
 
 #比較打對哪個字的函式
+class removeText():
+    def __init__(self, name):
+    self.name = name
+a = Animal("dog")  #建立一個名叫dog的Animal實體(物件)
+print(a.name)
+
 def compare():
+    global monsterHP
     for i in randomText:
         if(playerInput == i): #成功打入一樣的字母
-            removeText(i)
+            
+            removeNum = removeText(i,0)
+            monsterHP -= removeNum*5
             print('你打對',playerInput,'了')
+            print(monsterHP)
         else: #沒打對的話
             pass
 
 #專門刪除List內的字
-def removeText(i): #i = 真的字 j = 那個字的index
-    while(i in randomText):
-        j = randomText.index(i)
-        Text.remove(Text[j])
-        randomText.remove(i)
-        Text_dy.remove(Text_dy[j])
-        TextPosition.remove(TextPosition[j])
+def removeText(j,mode): #j=字的index mode=0畫面上全消 mode=1只消一個
+    Count = 0
+    if(mode = 0):
+        while(i in randomText):
+            j = randomText.index(i)
+            Text.remove(Text[j])
+            randomText.remove(i)
+            Text_dy.remove(Text_dy[j])
+            TextPosition.remove(TextPosition[j])
+            if(mode==1):
+                break
+            Count += 1
+        return Count
+    if(mode = 1):
+        
 
 #nonPygame side
 path = os.path.split(os.path.abspath(__file__))[0] #遊戲資料夾位址
@@ -48,13 +70,11 @@ pygame.font.init()
 pygame.display.set_caption("Type Hero")
 resolution = (1024, 768)
 screen = pygame.display.set_mode(resolution)
-background = pygame.Surface(screen.get_size())
-#pygame.event.pump()
-#pygame.event.set_blocked(pygame.MOUSEMOTION)
 
 
 
 #background
+background = pygame.Surface(screen.get_size())
 background.fill((125,125,125))
 background = background.convert()
 
@@ -86,8 +106,10 @@ monsterChange = monsterChange.convert_alpha()
 monsterCenter = [resolution[0]-monsterWidth/2, resolution[1]/2]
 monster_dy = 3 #移動速度
 
-#monsters health
-
+#monsters HP
+monsterHP = 100
+HP = pygame.Surface((200,20))
+HP.fill((0,0,0))
 #game clock
 clock = pygame.time.Clock()
 lastTick = 0
@@ -104,7 +126,7 @@ TextPosition[0][0] -= 50
 playerInput = ''
 while running:
     clock.tick(30) #fps
-    pygame.key.start_text_input()
+    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -121,7 +143,7 @@ while running:
                 else:
                     playerInput += event.unicode
         
-    screen.blit(background, (0,0))
+    screen.blit(background, (0,0)) #背景在這
     
     #monster 移動&反彈
     monsterCenter[1] += monster_dy
@@ -167,9 +189,12 @@ while running:
             TextPosition[j][1] += Text_dy[j]
             screen.blit(i, (TextPosition[j][0], TextPosition[j][1])) #字移動
             if (TextRight <= 0):
-                removeText(randomText[j])
+                print(randomText)
                 print(randomText[j],'被丟棄了')
-        
+                removeText(randomText[j],1)
+                print(randomText)
+    #monsterHPbar
+    screen.blit(HP, (monsterRect.left + 55, monsterRect.top - 20))
 
     pygame.display.update()
     if (len(Text) > 10): #字的數量超過10就pop掉一個
